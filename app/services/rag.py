eab273ca0c9e3cff5f8bbb8e35a746b1edc3b19f
Ferguson Jezreel <anojafa24@gmail.com>
2026-06-25 13:15:52 +0000

    feat: remove deprecated AI agent code, update LLM service, add health Q&A function

--- app/services/rag.py
+++ app/services/rag.py
@@ -1,7 +1,8 @@
 from app.services.llm_service import llm
 
 def ask_health_question(question: str):
-
+    # CDC = "https://www.cdc.gov/health-topics.html"
+    # WHO = "https://www.who.int/"
     prompt = f"""
     You are a health education assistant.
 
@@ -10,6 +11,8 @@ def ask_health_question(question: str):
 
     Give educational information only.
     Do not diagnose diseases.
+    
+    Your response should be accurate or precise and short. But make sure is very useful for the user
     """
 
     response = llm.invoke(prompt)
